af248fdb06ddf14c132dab1399221ad148340518
pablo-insfran <pablo98dani@gmail.com>
2023-02-01 13:34:57 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
+import axios from 'axios';
+import React, { useState } from 'react';
+
 import './App.css';
 
 function App() {
+
+  const [responseData, setResponseData] = useState([]);
+
+  const consulpokemon = async () => {
+    try {
+      const response = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0')
+      setResponseData(response.data.results);
+    }
+    catch (error) {
+      console.log(error);
+    }
+  };
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <button onClick={() => consulpokemon()} type="button" className="btn btn-secondary">Fetch Pokemon</button>
+      <ol className="list-group list-group-numbered">
+        {responseData.map((pokemon) =>
+          <li className="list-group-item">{pokemon.name}</li>
+        )}
+      </ol>
+    </>
   );
 }
 
